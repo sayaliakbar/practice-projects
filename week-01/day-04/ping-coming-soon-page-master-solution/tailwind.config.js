42045829258a61ad11f4e86a7ee4657fb3b46192
sayakbarali <aliakbar_intern@netixsol.com>
2024-11-26 22:56:15 +0500

    responsiveness configured

--- week-01/day-04/ping-coming-soon-page-master-solution/tailwind.config.js
+++ week-01/day-04/ping-coming-soon-page-master-solution/tailwind.config.js
@@ -19,6 +19,22 @@ module.exports = {
         900: "900",
       },
     },
+    screens: {
+      sm: "430px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 };
