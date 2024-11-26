0f5be1f45af7bf8c2f706ac7b22695a0181c5d24
sayakbarali <aliakbar_intern@netixsol.com>
2024-11-26 16:13:37 +0500

    Home page updated

--- week-01/day-04/ping-coming-soon-page-master-solution/tailwind.config.js
+++ week-01/day-04/ping-coming-soon-page-master-solution/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // Add Libre Franklin font under a custom name
+        libre: ['"Libre Franklin"', "sans-serif"],
+      },
+      fontWeight: {
+        100: "100",
+        200: "200",
+        300: "300",
+        400: "400",
+        500: "500",
+        600: "600",
+        700: "700",
+        800: "800",
+        900: "900",
+      },
+    },
   },
   plugins: [],
 };
