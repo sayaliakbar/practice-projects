b91ac1ba5822eb353778fd6ed6ff59ed43cb3997
sayakbarali <aliakbar_intern@netixsol.com>
2024-11-27 18:08:22 +0500

    web design ready

---
+++ week-01/day-06/intro-component-with-signup-form-solution/dist/assets/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("claim");
+button.addEventListener("click", doSomething);
+
+function doSomething() {
+  alert("Hello Worlds ");
+}
--- week-01/day-06/intro-component-with-signup-form-solution/tailwind.config.js
+++ week-01/day-06/intro-component-with-signup-form-solution/tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          red: "hsl(0, 100%, 74%)",
+          green: "hsl(154, 59%, 51%)",
+        },
+        accent: {
+          blue: "hsl(248, 32%, 49%)",
+        },
+        neutral: {
+          darkBlue: "hsl(249, 10%, 26%)",
+          grayishBlue: "hsl(246, 25%, 77%)",
+        },
+      },
+      fontFamily: {
+        // Add Inter font under a custom name
+        Poppins: ['"Poppins"', "sans-serif"],
+      },
+      fontWeight: {
+        400: "400",
+        500: "500",
+        600: "600",
+        700: "700",
+      },
+    },
   },
   plugins: [],
 };
