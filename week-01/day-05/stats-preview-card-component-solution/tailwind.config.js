d415f6ba5170b3b43169bdfefc64dc57345ef875
sayakbarali <aliakbar_intern@netixsol.com>
2024-11-27 11:02:55 +0500

    font styles added

--- week-01/day-05/stats-preview-card-component-solution/tailwind.config.js
+++ week-01/day-05/stats-preview-card-component-solution/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // Add Inter font under a custom name
+        inter: ['"Inter"', "sans-serif"],
+        lexendDeca: ["Lexend Deca", "sans-serif"],
+      },
+      fontWeight: {
+        100: "100",
+        200: "200",
+        300: "300",
+        400: "400",
+        500: "500",
+        600: "600",
+        700: "700",
+        800: "800",
+        900: "900",
+      },
+    },
   },
   plugins: [],
 };
