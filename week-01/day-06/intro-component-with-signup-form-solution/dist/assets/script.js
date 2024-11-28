d05819247ed8698786e350983f3bb015872be11d
sayakbarali <aliakbar_intern@netixsol.com>
2024-11-28 18:01:20 +0500

    error icon added

--- week-01/day-06/intro-component-with-signup-form-solution/dist/assets/script.js
+++ week-01/day-06/intro-component-with-signup-form-solution/dist/assets/script.js
@@ -7,29 +7,52 @@ function handleSubmit(event) {
 }
 
 function validateForm() {
+  //   let errorIcon = document.getElementById("passwordErrorIcon");
+  //   errorIcon.classList.remove("hidden");
+
   // Get form fields
   const fields = [
-    { id: "firstName", errorId: "firstNameError" },
-    { id: "lastName", errorId: "lastNameError" },
-    { id: "email", errorId: "emailError", validate: validateEmail },
-    { id: "password", errorId: "passwordError" },
+    {
+      id: "firstName",
+      errorId: "firstNameError",
+      errorIconId: "firstNameErrorIcon",
+    },
+    {
+      id: "lastName",
+      errorId: "lastNameError",
+      errorIconId: "lastNameErrorIcon",
+    },
+    {
+      id: "email",
+      errorId: "emailError",
+      validate: validateEmail,
+      errorIconId: "emailErrorIcon",
+    },
+    {
+      id: "password",
+      errorId: "passwordError",
+      errorIconId: "passwordErrorIcon",
+    },
   ];
 
   let isValid = true;
 
-  fields.forEach(({ id, errorId, validate }) => {
+  fields.forEach(({ id, errorId, validate, errorIconId }) => {
     const field = document.getElementById(id);
     const error = document.getElementById(errorId);
+    const errorIcon = document.getElementById(errorIconId);
 
     if (!field.value || (validate && !validate(field.value))) {
       field.classList.remove("border-solid");
       field.classList.add("border-primary-red");
       error.classList.remove("hidden");
+      errorIcon.classList.remove("hidden");
       isValid = false;
     } else {
       field.classList.remove("border-primary-red");
       field.classList.add("border-solid");
       error.classList.add("hidden");
+      errorIcon.classList.add("hidden");
     }
   });
 
