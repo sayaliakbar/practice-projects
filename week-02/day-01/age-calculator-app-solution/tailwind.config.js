18582c65f79fa26aaa2ebfeac0be9b6f929d88c6
sayakbarali <aliakbar_intern@netixsol.com>
2024-12-01 15:55:07 +0500

    files initialised

--- week-02/day-01/age-calculator-app-solution/tailwind.config.js
+++ week-02/day-01/age-calculator-app-solution/tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          purple: "hsl(259, 100%, 65%)",
+          lightRed: "hsl(0, 100%, 67%)",
+        },
+        neutral: {
+          white: "hsl(0, 0%, 100%)",
+          offWhite: "hsl(0, 0%, 94%)",
+          lightGrey: "hsl(0, 0%, 86%)",
+          smokeyGrey: "hsl(0, 1%, 44%)",
+          offBlack: "hsl(0, 0%, 8%)",
+        },
+      },
+      fontSize: {
+        body: "32px", // Font size for inputs
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontWeight: {
+        400: "400",
+        700: "700",
+        800: "800",
+      },
+    },
   },
   plugins: [],
 };
