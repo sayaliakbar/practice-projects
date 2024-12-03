80c6e874e5cada635a978d29d9c0644da98b421a
sayakbarali <aliakbar_intern@netixsol.com>
2024-12-03 14:55:07 +0500

    tailwind initialised

--- week-02/day-03/interactive-rating-component-solution/tailwind.config.js
+++ week-02/day-03/interactive-rating-component-solution/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { overpass: ["Overpass", "sans-serif"] },
+      fontWeight: { 400: "400", 700: "700" },
+      colors: {
+        primary: {
+          orange: "hsl(25, 97%, 53%)",
+        },
+        neutral: {
+          "light-grey": "hsl(217, 12%, 63%)",
+          "dark-blue": "hsl(213, 19%, 18%)",
+          "very-dark-blue": "hsl(216, 12%, 8%)",
+        },
+      },
+      fontSize: {
+        base: "15px",
+      },
+    },
   },
   plugins: [],
 };
