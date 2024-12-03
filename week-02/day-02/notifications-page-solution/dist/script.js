4aade142f7d07aecf1503f853db516cea12ecc97
sayakbarali <aliakbar_intern@netixsol.com>
2024-12-03 13:57:51 +0500

    remove background

--- week-02/day-02/notifications-page-solution/dist/script.js
+++ week-02/day-02/notifications-page-solution/dist/script.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+function handleSubmit(event) {
+  event.preventDefault();
+  markRead();
+}
+
+function markRead() {
+  let notificationNumber = document.getElementById("notificationNumber");
+  notificationNumber.innerHTML = 0;
+  let notificationIcons = document.getElementsByClassName("notificationIcon");
+  for (let notificationIcon of notificationIcons) {
+    notificationIcon.classList.add("hidden");
+    notificationIcon.classList.remove("inline-block");
+  }
+  let profiles = document.getElementsByClassName("profiles");
+  for (let profile of profiles) {
+    profile.classList.remove("bg-neutral-light-blue-1");
+  }
+}
