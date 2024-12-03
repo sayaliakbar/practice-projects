b4fdb1181b989c71eaf9ca08c25c7e10111afa55
sayakbarali <aliakbar_intern@netixsol.com>
2024-12-03 14:15:47 +0500

    minor changes

--- week-02/day-02/notifications-page-solution/dist/script.js
+++ week-02/day-02/notifications-page-solution/dist/script.js
@@ -4,15 +4,20 @@ function handleSubmit(event) {
 }
 
 function markRead() {
-  let notificationNumber = document.getElementById("notificationNumber");
-  notificationNumber.innerHTML = 0;
-  let notificationIcons = document.getElementsByClassName("notificationIcon");
-  for (let notificationIcon of notificationIcons) {
-    notificationIcon.classList.add("hidden");
-    notificationIcon.classList.remove("inline-block");
+  const notificationNumber = document.querySelector("#notificationNumber");
+  const notificationIcons = document.querySelectorAll(".notificationIcon");
+  const profiles = document.querySelectorAll(".profiles");
+
+  if (notificationNumber) {
+    notificationNumber.textContent = 0;
   }
-  let profiles = document.getElementsByClassName("profiles");
-  for (let profile of profiles) {
+
+  notificationIcons.forEach((icon) => {
+    icon.classList.add("hidden");
+    icon.classList.remove("inline-block");
+  });
+
+  profiles.forEach((profile) => {
     profile.classList.remove("bg-neutral-light-blue-1");
-  }
+  });
 }
