0db3b1f67037948cec9b5031099eec1c23f269be
sayakbarali <aliakbar_intern@netixsol.com>
2024-12-03 19:57:26 +0500

    draft code writter

--- week-02/day-04/tip-calculator-app-solution/tailwind.config.js
+++ week-02/day-04/tip-calculator-app-solution/tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { spaceMono: ["Space Mono", "monospace"] },
+      fontWeight: { 700: "700" },
+      colors: {
+        primary: {
+          cyan: "hsl(172, 67%, 45%)",
+        },
+        neutral: {
+          "very-dark-cyan": "hsl(183, 100%, 15%)",
+          "dark-grayish-cyan": "hsl(186, 14%, 43%)",
+          "grayish-cyan": "hsl(184, 14%, 56%)",
+          "light-grayish-cyan": "hsl(185, 41%, 84%)",
+          "very-light-grayish-cyan": "hsl(189, 41%, 97%)",
+        },
+      },
+      fontSize: {
+        formInputs: "24px",
+      },
+    },
   },
   plugins: [],
 };
