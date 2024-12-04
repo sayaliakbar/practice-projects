70ebd47768b2a39c67c9ffc1fc5f7b369e6f21a4
sayakbarali <aliakbar_intern@netixsol.com>
2024-12-04 23:44:33 +0500

    calculation completed

--- week-02/day-04/tip-calculator-app-solution/dist/script.js
+++ week-02/day-04/tip-calculator-app-solution/dist/script.js
@@ -1,6 +1,7 @@
 let tips = document.querySelectorAll(".tip");
 let isFirstSelection = true;
 let selectedTip;
+let tip = 0;
 
 tips.forEach((tip) => {
   tip.addEventListener("click", select);
@@ -13,26 +14,19 @@ function select(event) {
   }
 
   // Update selected rating and apply styles
-  selectedTip = event.currentTarget;
+  selectedTip = event.target;
   if (event.target.nodeName != "INPUT") {
     selectedTip.classList.add("bg-primary-cyan", "text-black");
+
+    let number = selectedTip.textContent.match(/\d+/)[0];
+    tip = number / 100;
+    tip *= bill.value;
   }
 
   // Mark as no longer the first selection
   isFirstSelection = false;
 }
 
-// function handleSubmit() {
-//   let people = document.getElementById("people");
-
-//   if (!people.innerHTML) {
-//     error.classList.remove("hidden");
-//     people.classList.add("border-red-600");
-//   } else {
-//     error.classList.add("hidden");
-//     people.classList.remove("border-red-600");
-//   }
-// }
 const bill = document.getElementById("bill");
 const people = document.getElementById("people");
 const error = document.getElementById("error");
@@ -45,18 +39,45 @@ people.addEventListener("input", output);
 function reset() {
   bill.value = "";
   people.value = "";
+  tipAmount.innerText = "$0.00";
+  totalAmount.innerText = "$0.00";
+  select();
 }
 
 function output() {
-  if (people.value === "" || people.value === 0) {
+  if (people.value == 0) {
     error.classList.remove("hidden");
-  } else {
-    if (bill.value === "") {
-      tipAmount.innerHTML = "$0.00";
-    } else {
-      tipAmount.innerHTML = bill.value / people.value;
-    }
+    people.classList.add("border-red-600");
+  }
+  if (people.value === "") {
     error.classList.add("hidden");
-    console.log(bill.value / people.value);
+    people.classList.remove("border-red-600");
+    tipAmount.innerText = "$0.00";
+    totalAmount.innerText = "$0.00";
+  }
+
+  // When both values are empty
+  if (bill.value === "" && people.value === "") {
+    tipAmount.innerText = "$0.00";
+    totalAmount.innerText = "$0.00";
+  }
+  // When both values are 0
+  if (bill.value == 0 && people.value == 0) {
+    tipAmount.innerText = "$0.00";
+    totalAmount.innerText = "$0.00";
+  }
+  // When both values are greater then 1
+  if (bill.value > 0 && people.value > 0) {
+    let totalBill = parseInt(bill.value, 10) + tip;
+    let tipam = tip / people.value;
+    let billam = totalBill / people.value;
+    tipAmount.innerText = tipam.toFixed(2);
+    totalAmount.innerText = billam.toFixed(2);
+    console.log(tip);
+  }
+  //When bill value is empty or 0
+  if (bill.value === "" || bill.value == 0) {
+    tipAmount.innerText = "$0.00";
+    totalAmount.innerText = "$0.00";
   }
 }
