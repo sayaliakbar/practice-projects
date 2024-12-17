7e0a574d379f44b986d6c07cf2ad95af9f53c13c
sayakbarali <aliakbar_intern@netixsol.com>
2024-12-17 19:51:14 +0500

    backend initialised

---
+++ week-03/day-06/todo-app-solution/backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const corsOptions = {
+  origin: ["https://localhost:5173"],
+};
+
+const app = express();
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Allow requests from this specific origin
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Specify allowed methods
+  res.header("Access-Control-Allow-Headers", "Content-Type"); // Specify allowed headers
+  next();
+});
+
+app.get("/api", (req, res) => {
+  res.json({ users: ["userOne", "userTwo", "userThree"] });
+});
+
+app.listen(8080, () => {
+  console.log("Server started on port 5000");
+});
--- week-03/day-06/todo-app-solution/frontend/src/App.jsx
+++ week-03/day-06/todo-app-solution/frontend/src/App.jsx
@@ -1,12 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import bgDesktopLight from "./assets/images/bg-desktop-light.jpg";
 import bgMobileLight from "./assets/images/bg-mobile-light.jpg";
 import bgDesktopDark from "./assets/images/bg-desktop-dark.jpg";
+
 import bgMobileDark from "./assets/images/bg-mobile-dark.jpg";
 import TodoContainer from "./components/TodoContainer";
 
 function App() {
+  const [array, setArray] = useState([]);
+
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8080/api");
+    setArray(response.data.users);
+    console.log(response.data.users);
+  };
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
   return (
     <>
+      <div>
+        <h1>{array}</h1>
+        {array.map((user, index) => {
+          <div>
+            <h1>{user}</h1>
+            <br />
+          </div>;
+        })}
+      </div>
+
       <div className="w-full flex items-center justify-center relative ">
         {/* Desktop Light Mode */}
         <picture className="w-full dark:hidden">
