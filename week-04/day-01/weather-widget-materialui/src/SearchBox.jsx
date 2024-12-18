4e377eb9428494301b69424cf4ad5e20e12d938d
sayakbarali <aliakbar_intern@netixsol.com>
2024-12-18 18:15:31 +0500

    response of weather got

--- week-04/day-01/weather-widget-materialui/src/SearchBox.jsx
+++ week-04/day-01/weather-widget-materialui/src/SearchBox.jsx
@@ -5,12 +5,33 @@ export default function SearchBox() {
   let [city, setCity] = useState("");
   let [label, setLabel] = useState("City");
 
+  let API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  let API_KEY = "2f8568705ee1f733caa322423eb9441e";
+
+  let getWeatherInfo = async () => {
+    let response = await fetch(
+      `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    let jsonResponse = await response.json();
+
+    let result = {
+      name: jsonResponse.name,
+      temp: jsonResponse.main.temp,
+      tempMin: jsonResponse.main.temp_min,
+      tempMax: jsonResponse.main.temp_max,
+      humidity: jsonResponse.main.humidity,
+      feelsLike: jsonResponse.main.feels_like,
+      weather: jsonResponse.weather[0].description,
+    };
+    console.log(result);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (city) {
-      console.log(city);
       setLabel("City");
       setCity("");
+      getWeatherInfo();
     } else {
       setLabel("Please enter city!");
     }
