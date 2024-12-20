34abe4a3ad6bbb11e1237ecf1c53d368c7fbd4d2
sayakbarali <aliakbar_intern@netixsol.com>
2024-12-20 17:08:14 +0500

    files initialised

---
+++ week-04/day-02/nft-portfolio-website/src/App.jsx
@@ -0,0 +1,22 @@
+import {
+  Collaboration,
+  Collection,
+  Community,
+  Featured,
+  Footer,
+  Home,
+  Newsletter,
+  Sale,
+} from "./containers";
+import { Header } from "./components";
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ week-04/day-02/nft-portfolio-website/src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Collections } from "./collections/Collections";
+export { default as Footer } from "./footer/Footer";
+export { default as Header } from "./header/Header";
+export { default as Hero } from "./hero/Hero";
+export { default as Highlight } from "./highlight/Highlight";
+export { default as Newsletter } from "./newsletter/Newsletter";
+export { default as Partners } from "./partners/Partners";
+export { default as Sale } from "./sale/Sale";
