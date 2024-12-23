cb3648671247a03021b2fd8603ef83d230e1df67
sayakbarali <aliakbar_intern@netixsol.com>
2024-12-23 17:23:49 +0500

    collections added

--- week-04/day-02/nft-portfolio-website/src/components/highlight/Heading.jsx
+++ week-04/day-02/nft-portfolio-website/src/components/highlight/Heading.jsx
@@ -1,12 +1,9 @@
-export default function Heading() {
+export default function Heading({ heading, title, description }) {
   return (
     <div className="flex flex-col items-center  gap-4 mx-48">
-      <h6 className="heading-bold-h6 text-accent-cyan ">Featured</h6>
-      <h2 className="heading-bold-h2">Hot Trending On This Week from Yorfy</h2>
-      <p className="paragraph-body text-text-disabled">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
+      <h6 className="heading-bold-h6 text-accent-cyan ">{heading}</h6>
+      <h2 className="heading-bold-h2">{title}</h2>
+      <p className="paragraph-body text-text-disabled">{description}</p>
     </div>
   );
 }
--- week-04/day-02/nft-portfolio-website/src/components/highlight/Highlight.jsx
+++ week-04/day-02/nft-portfolio-website/src/components/highlight/Highlight.jsx
@@ -3,7 +3,11 @@ import HighlightNFT from "./HighlightNFT";
 export default function Highlight() {
   return (
     <div className="mx-[4.5rem] text-center flex flex-col items-center gap-10">
-      <Heading></Heading>
+      <Heading
+        heading="Featued"
+        title="Hot Trending On This Week from Yorfy"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      ></Heading>
       <HighlightNFT></HighlightNFT>
     </div>
   );
