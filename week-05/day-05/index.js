2b9793edc911fd388de06f9c9f75f0eff1190736
sayakbarali <aliakbar_intern@netixsol.com>
2025-01-03 10:58:25 +0500

    authentication completed

--- week-05/day-05/index.js
+++ week-05/day-05/index.js
@@ -1,37 +1,67 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
 const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const methodOverride = require("method-override");
+
+const users = [];
 
 const initialisePassport = require("./passport-config");
 
-// initialisePassport(passport);
+initialisePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(flash());
 
-let users = [];
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "default_secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "World" });
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
 });
 
-app.get("/login", (req, res) => {
+app.get("/login", checkNotAuthenticated, (req, res) => {
   res.render("login.ejs");
 });
 
-app.get("/register", (req, res) => {
+app.get("/register", checkNotAuthenticated, (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/login", (req, res) => {
-  res.send("Login successful");
-});
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
-app.post("/register", (req, res) => {
+app.post("/register", checkNotAuthenticated, async (req, res) => {
   try {
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     users.push({
       id: Date.now().toString(),
       name: req.body.name,
@@ -42,7 +72,25 @@ app.post("/register", (req, res) => {
   } catch (error) {
     res.redirect("/register");
   }
-  console.log(users);
 });
 
+app.delete("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+// The server is listening on port 3000
 app.listen(3000);
