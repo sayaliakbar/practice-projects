5f41d1053c3921519109193e4af2219678682c4e
sayakbarali <aliakbar_intern@netixsol.com>
2025-01-07 14:15:48 +0500

    minor changes

--- week-06/day-01/share-prompts/components/Nav.jsx
+++ week-06/day-01/share-prompts/components/Nav.jsx
@@ -6,19 +6,20 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState([null]);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
-      setProviders(response);
+      seProviders(response);
     };
     setProviders();
   }, []);
 
   return (
+    // {Desktop Navigation}
     <nav className="flex-between w-full mb-16 pt-3">
       <Link className="flex gap-2 flex-center" href="/">
         <Image
@@ -30,10 +31,10 @@ const Nav = () => {
         ></Image>
         <p className="logo_text">Promptopia</p>
       </Link>
-
+      {alert(providers)}
       {/* {Mobile Navigation} */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -67,7 +68,7 @@ const Nav = () => {
         )}
       </div>
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div>
             <Image
               src="/assets/images/logo.svg"
--- week-06/day-01/share-prompts/models/user.js
+++ week-06/day-01/share-prompts/models/user.js
@@ -16,10 +16,7 @@ const userSchema = new Schema({
     required: [true, "Email is required"],
     unique: [true, "Email already exists"],
   },
-  password: {
-    type: String,
-    required: true,
-  },
+
   image: {
     type: String,
   },
