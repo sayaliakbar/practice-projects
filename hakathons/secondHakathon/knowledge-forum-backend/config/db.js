a09c0901d7677c27b659fe056a875ecbe19e043b
sayakbarali <aliakbar_intern@netixsol.com>
2025-01-14 00:25:03 +0500

    mongodb development mode added

--- hakathons/secondHakathon/knowledge-forum-backend/config/db.js
+++ hakathons/secondHakathon/knowledge-forum-backend/config/db.js
@@ -2,12 +2,16 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      process.env.NODE_ENV === "development"
+        ? process.env.MONGO_URI_DEV
+        : process.env.MONGO_URI
+    );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
+    process.exit(1); // Exit process with failure
   }
 };
 
